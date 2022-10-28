5975708f557718ea486e8ea91ae94f1cdb4720a0
Fixxoo <rajpalmal40@gmail.com>
2022-10-28 23:41:09 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+            "./index.html",
+            "./trial.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
